ceb3db69f86b057ad84cb90cf5a07d1ca040fa5b
Dmitriy Savinykh <biostif13@gmail.com>
2022-08-30 00:17:58 +0300

    Add transfer in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val minCommission = 35
+    val commission = 0.75
+    val coins = 0.01
+    val amount = 100_59
+    val rubSymbol = "\u20BD"
+    if (amount - (amount*commission) < minCommission/coins)
+        println("Сумма перевода составит: ${(amount + (minCommission/coins)) * coins}$rubSymbol")
+    else
+        println("Сумма перевода составит: ${((amount + (amount*commission)).toInt()) * coins}$rubSymbol")
 }
